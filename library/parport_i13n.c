fd58319cb8e9cfcd88650c6b64f6592471281a40
Roger Light <roger@atchoo.org>
2014-07-10 14:30:33 +0100

    indent -linux

--- library/parport_i13n.c
+++ library/parport_i13n.c
@@ -8,76 +8,87 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-int parport_dev=0;
-int open_count=0;
-unsigned char parport_data=0;
+int parport_dev = 0;
+int open_count = 0;
+unsigned char parport_data = 0;
 
-void parport_open(char * name){
-	if(open_count<1){	/* should be able to open parport multiple times, need to keep track */
-		parport_dev=open(name,O_RDWR);
-		if(parport_dev<0){
-			printf("parport: fatal error cannot open \"%s\"\n",name);
-			printf("Error number is %d\n",errno);
+void parport_open(const char *name)
+{
+	if (open_count < 1) {	/* should be able to open parport multiple times, need to keep track */
+		parport_dev = open(name, O_RDWR);
+		if (parport_dev < 0) {
+			printf("parport: fatal error cannot open \"%s\"\n",
+			       name);
+			printf("Error number is %d\n", errno);
 			perror("parport_open: error");
 			exit(1);
-			}
+		}
 		/* set up exclusive rights to the parport */
-		ioctl(parport_dev,PPEXCL); /* note that robi doesn't use this....*/
-		ioctl(parport_dev,PPCLAIM);
+		ioctl(parport_dev, PPEXCL);	/* note that robi doesn't use this.... */
+		ioctl(parport_dev, PPCLAIM);
 		// for the shutters expt we have to leave the state unknow until we set it
 		//parport_data=0;
 		//parport_out(parport_data);
-		ioctl(parport_dev,PPRDATA,&parport_data);
-		}
+		ioctl(parport_dev, PPRDATA, &parport_data);
+	}
 	open_count++;
-	}
-	
-void parport_close(void){
-	if(open_count==1){
-		ioctl(parport_dev,PPRELEASE);
-		close(parport_dev);
-		}
-	open_count--;
-	}
+}
 
-void parport_out(unsigned char data){
-unsigned char there;
-	if(ioctl(parport_dev,PPWDATA,&data)==0)
-		parport_data=data;
+void parport_close(void)
+{
+	if (open_count == 1) {
+		ioctl(parport_dev, PPRELEASE);
+		close(parport_dev);
 	}
+	open_count--;
+}
+
+void parport_out(unsigned char data)
+{
+	unsigned char there;
+	if (ioctl(parport_dev, PPWDATA, &data) == 0) {
+		parport_data = data;
+	}
+}
 
 // these work with a bit mask - changing the 1s in the mask leaving the 0s
-void parport_bits_up(unsigned char bit){
-        parport_out(bit | parport_data);
-        }
-void parport_bits_down(unsigned char bit){
-        parport_out(~bit & parport_data);
-        }
+void parport_bits_up(unsigned char bit)
+{
+	parport_out(bit | parport_data);
+}
+
+void parport_bits_down(unsigned char bit)
+{
+	parport_out(~bit & parport_data);
+}
+
 // these set the bit 0-7 only
-void parport_bit_up(unsigned char bit){
-	parport_out((1<<bit) | parport_data);
-	}
-void parport_bit_down(unsigned char bit){
-	parport_out(~(1<<bit) & parport_data);
-	}
+void parport_bit_up(unsigned char bit)
+{
+	parport_out((1 << bit) | parport_data);
+}
+
+void parport_bit_down(unsigned char bit)
+{
+	parport_out(~(1 << bit) & parport_data);
+}
 
 // bits labelled 0-7
-void parport_bit_set(unsigned char bit,int val){
-	if(val)parport_bit_up(bit);
-	else parport_bit_down(bit);
+void parport_bit_set(unsigned char bit, int val)
+{
+	if (val) {
+		parport_bit_up(bit);
+	} else {
+		parport_bit_down(bit);
 	}
-unsigned char parport_value(){
-        return parport_data;
-        }
+}
 
-unsigned char parport_n2b(int n){
-        return 1 << n;
-        }
+unsigned char parport_value(void)
+{
+	return parport_data;
+}
 
-
-
-
-
-
-
-
+unsigned char parport_n2b(int n)
+{
+	return 1 << n;
+}
--- library/parport_i13n.h
+++ library/parport_i13n.h
@@ -1,15 +1,15 @@
 #ifndef _PARPORT_I13N_H_
 #define _PARPORT_I13N_H_
 
-void parport_open(char *);
+void parport_open(const char *name);
 void parport_close(void);
-void parport_out(unsigned char);
-void parport_bit_up(unsigned char);
-void parport_bits_up(unsigned char);
-void parport_bit_down(unsigned char);
-void parport_bits_down(unsigned char);
-void parport_bit_set(unsigned char, int);
+void parport_out(unsigned char data);
+void parport_bit_up(unsigned char bit);
+void parport_bits_up(unsigned char bit);
+void parport_bit_down(unsigned char bit);
+void parport_bits_down(unsigned char bit);
+void parport_bit_set(unsigned char bit, int val);
 unsigned char parport_value(void);
-unsigned char parport_n2b(int);
+unsigned char parport_n2b(int n);
 
 #endif
